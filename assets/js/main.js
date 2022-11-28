db93a12df57b0b93028657f6e557846af64229b8
Christian Mahnke <cmahnke@gmail.com>
2022-11-28 10:00:13 +0100

    Started animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,23 +5,10 @@ window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 import Headroom from 'headroom.js';
 window.Headroom = Headroom;
 
-(function ($) {
+$( document ).ready(function() {
   'use strict';
 
   var options = {};
   var header = new Headroom(document.querySelector("header"), options);
   header.init();
-
-})(jQuery);
-
-/*
-$(document).ready(function() {
-
-  var options = {};
-  var header = new Headroom(document.getElementById('header'), options);
-  header.init();
-
 });
-
-
-*/
