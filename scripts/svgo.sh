2fdfde4852c7e6bd477c00183505a7c677c0fc93
Christian Mahnke <cmahnke@gmail.com>
2022-12-03 17:16:01 +0100

    WIP

--- scripts/svgo.sh
+++ scripts/svgo.sh
@@ -19,3 +19,12 @@ yarn run svgo --config ./config/svgo.config.js -i "./Source Files/footer2.svg" -
 yarn run svgo --config ./config/svgo.config.js -i "./Source Files/footer3.svg" -o "./static/images/footer3.svg" --multipass
 yarn run svgo --config ./config/svgo.config.js -i "./Source Files/signature.svg" -o "./static/images/signature.svg" --multipass
 yarn run svgo --config ./config/svgo.config.js -i "./Source Files/favicon.svg" -o "./static/images/favicon.svg" --multipass
+
+# Content graphics
+yarn run svgo --config ./config/svgo.config.js -i "./Source Files/tiles/Archiv.svg" -o "./content/archive/archive.svg" --multipass
+
+yarn run svgo --config ./config/svgo.config.js -i "./Source Files/tiles/Dünen.svg" -o "./content/life/life.svg" --multipass
+
+yarn run svgo --config ./config/svgo.config.js -i "./Source Files/tiles/Gras.svg" -o "./content/conservancy/conservancy.svg" --multipass
+
+yarn run svgo --config ./config/svgo.config.js -i "./Source Files/tiles/Himmel-Meer.svg" -o "./content/art/art.svg" --multipass
