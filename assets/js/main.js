c77f56e7fd047ee15a2708e5916f632d1b4f865a
Christian Mahnke <cmahnke@gmail.com>
2022-12-11 01:49:35 +0100

    enabled audio player

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,9 @@ window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 import Headroom from 'headroom.js';
 window.Headroom = Headroom;
 
+import { addConsent } from './iframe-consent';
+import { audioplayer } from './audioplayer';
+
 $( document ).ready(function() {
   'use strict';
 
