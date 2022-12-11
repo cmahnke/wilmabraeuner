5891e6da30cb614e88abc6970ca65115e160fc05
Christian Mahnke <cmahnke@gmail.com>
2022-12-11 22:19:10 +0100

    Minor cosmetic changes

--- scripts/post-build/qr-codes.sh
+++ scripts/post-build/qr-codes.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 python themes/projektemacher-base/scripts/generate-qr-codes.py -i "./Source Files/icons/" -b circle
-python themes/projektemacher-base/scripts/generate-qr-codes.py -i "./Source Files/icons/" -b circle -o qrcode.pdf
+python themes/projektemacher-base/scripts/generate-qr-codes.py -i "./Source Files/icons/" -b circle -u -o qrcode.pdf
