c3f12d7a8f99b06ce34acb1f0547c842bc9428d6
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-08-15 20:52:09 +0200

    Add python dependency

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -4,6 +4,7 @@ set -e
 
 CTX_PATH="$(dirname $(realpath $0))"
 
+$CTX_PATH/../themes/projektemacher-base/scripts/github/jxl-0.11.1.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/docker-images.sh
